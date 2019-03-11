349f7294fdbca0b152f3b78e9be4b3c06d8863cd
Manuel Chacón De Dios <mch738@gmail.com>
2019-03-11 21:56:31 +0100

    25/02/19

---
+++ PHP/CLASES/Clase_25-02-19/segunda.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    <?php
+        //(nombrecoockie,contenidocookie,tiempo de expiracion)
+        setcookie("numbreusu","MANUEL", 0);
+    ?>
+    <p>
+        <a href="primera.php">Ir a primera</a>
+    </p>
+</body>
+</html>
